2647cdee1d5a64d19cbd6fc077da644ba5247e60
Nicolas Renaux <nickrenaux@gmail.com>
2024-02-13 20:48:34 -0300

    Instalando filtro de pesquisa

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((videos) =>
-    videos.forEach((video)=> {
-        containerVideos.innerHTML += `
-        <li class="videos__item">
-            <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                <div class="descricao-video">
-                 	<img class="img-canal" src = "${video.imagem}" alt="Logo do Canal">
-                 	<h3 class="titulo-video">${video.titulo}</h3>
-                         <p class="titulo-canal">${video.descricao}</p>
-                 </div>
-              
-        </li>
-        `;
-    
-    })
-)
+async function buscarEMostrarVideos() {
+    try {
+        const busca = await fetch("http://localhost:3000/videos")
+        const videos = await busca.json()
+            videos.forEach((video)=> {
+                containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                        <div class="descricao-video">
+                            <img class="img-canal" src = "${video.imagem}" alt="Logo do Canal">
+                            <h3 class="titulo-video">${video.titulo}</h3>
+                                <p class="titulo-canal">${video.descricao}</p>
+                        </div>
+                    
+                </li>
+                `;
+            
+            })
+    } catch (error) {
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os videos: ${error}</p>`;
+    }
+}
 
-.catch((error => {
-    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos... "${error}"</p>`
-}))
+buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector('.pesquisar__input');
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa(){
+    const videos = document.querySelectorAll('.videos__item');
+
+    if(barraDePesquisa.value != ""){
+        for(let video of videos){
+            let titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+            let filtro = barraDePesquisa.value.toLowerCase();
+
+            if(!titulo.includes(filtro)){
+                video.style.display = "none";
+            } else{
+                video.style.display = "block";
+            }
+        }
+    } else{
+        video.style.display = "block";
+    }
+}
